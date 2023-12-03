d650f65f426dcb2d0e7834659d5c62eabaf95b62
Hugo de Paula-Oliveira <hugodpaulaoliveira@gmail.com>
2023-12-03 13:33:17 -0300

    Update ANOVA.c

--- ANOVA.c
+++ ANOVA.c
@@ -6,42 +6,42 @@
 #define MAX_AMOSTRAS 10
 
 // 1- Calcular a média dos grupos (vetores)
-double calc_media(double data[], int size) {
-    double sum = 0.0;
-    for (int i = 0; i < size; i++) {
-        sum += data[i];
+double calc_media(double dados[], int tamanho) {
+    double soma = 0.0;
+    for (int i = 0; i < tamanho; i++) {
+        soma += dados[i];
     }
-    return sum / size;
+    return soma / tamanho;
 }
 
 // 2- Estimar a soma dos quadrados
-double calc_som_quadrados(double data[], int size, double mean) {
-    double sum = 0.0;
-    for (int i = 0; i < size; i++) {
-        sum += pow(data[i] - mean, 2);
+double calc_som_quadrados(double dados[], int tamanho, double media) {
+    double soma = 0.0;
+    for (int i = 0; i < tamanho; i++) {
+        soma += pow(dados[i] - media, 2);
     }
-    return sum;
+    return soma;
 }
 
 // 3- Executar ANOVA one-way
-void ANOVA(double data[MAX_GRUPOS][MAX_AMOSTRAS], int num_grupos, int tam_grupos) {
+void ANOVA(double dados[MAX_GRUPOS][MAX_AMOSTRAS], int num_grupos, int tam_grupos) {
     // Média Geral 
     double med_geral = 0.0;
     for (int i = 0; i < num_grupos; i++) {
-        med_geral += calc_media(data[i], tam_grupos);
+        med_geral += calc_media(dados[i], tam_grupos);
     }
     med_geral /= num_grupos;
 
     // Soma dos Quadrados (SQ)
     double som_quad_entre = 0.0;  //som_quad_entre = Soma dos quadrados entre os grupos (par-a-par)
     for (int i = 0; i < num_grupos; i++) {
-        double med_grupo = calc_media(data[i], tam_grupos);
+        double med_grupo = calc_media(dados[i], tam_grupos);
         som_quad_entre += tam_grupos * pow(med_grupo - med_geral, 2);
     }
 
     double som_quad_dentre = 0.0;   //som_quad_dentre = Soma dos quadrados dentro dos grupos
     for (int i = 0; i < num_grupos; i++) {
-        som_quad_dentre += calc_som_quadrados(data[i], tam_grupos, calc_media(data[i], tam_grupos));
+        som_quad_dentre += calc_som_quadrados(dados[i], tam_grupos, calc_media(dados[i], tam_grupos));
     }
 
     // Graus de liberdade (GL)
@@ -68,13 +68,13 @@ int main() {
     int num_grupos = 3;
     int tam_grupos = 10;
 
-    double data[MAX_GRUPOS][MAX_AMOSTRAS] = {
+    double dados[MAX_GRUPOS][MAX_AMOSTRAS] = {
         {76, 77, 77, 81, 82, 82, 83, 84, 85, 89},
         {81, 82, 83, 83, 83, 84, 87, 90, 92, 93},
         {77, 80, 82, 88, 89, 90, 94, 95, 95, 98}
     };
 
-    ANOVA(data, num_grupos, tam_grupos);
+    ANOVA(dados, num_grupos, tam_grupos);
 
     return 0;
 }
